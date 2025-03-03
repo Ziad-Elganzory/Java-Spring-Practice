60aab3d3304aa86ab8f9eecaefa2a71cd773b6e5
Ziad-Elganzory <ziadmohamed770@outlook.com>
2025-03-03 23:47:00 +0200

    Spring Overview

---
+++ 01-spring-boot-overview/SpringApp/src/main/java/com/codezury/springboot/demo/MyApp/rest/FunRestController.java
@@ -0,0 +1,12 @@
+package com.codezury.springboot.demo.MyApp.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+    @GetMapping("/")
+    public String sayHello(){
+        return "Hello Test!";
+    }
+}
